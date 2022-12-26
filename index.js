9a7088bc2ba8e84298d99e46446fff6bd1cabf50
Hongyu An <hongyuan22222@gmail.com>
2022-12-26 20:59:09 +0000

    polish api scraper

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 function extract(object) {
     var requestTime = (new Date()).getTime();
     var busID = object["vehicleId"];
@@ -20,47 +19,43 @@ async function getTFL(url) {
     });
 }
 
-
-// ************
-
 getTFL('https://api.tfl.gov.uk/Line/154/Arrivals/490012554E?modesFilter=tube&oysterOnly=false').then(function(arr) {
     let usefulData = arr;
+
     const express = require("express");
-const {google} = require("googleapis");
- 
-const app = express();
- 
-app.get("/", async (req, res) => {
-    const auth = new google.auth.GoogleAuth({
-        keyFile: "credentials.json",
-        scopes: "https://www.googleapis.com/auth/spreadsheets",
-    })
- 
-    // Create client instance for auth
-    const client = await auth.getClient();
- 
-    // Instance of Google Sheets API
-    const googleSheets = google.sheets({version: "v4", auth: client});
- 
-    const spreadsheetId = "1oSz2GirT0q9jaIurljRSZQDla6WNESmAIBDbJFC-guc";
- 
-    // Write
-    await googleSheets.spreadsheets.values.append({
-        auth,
-        spreadsheetId,
-        range: "Sheet1!A:D",
-        valueInputOption: "RAW",
-        resource: {
-            values: usefulData,
-        },
+
+    const { google } = require("googleapis");
+    
+    const app = express();
+    
+    app.get("/", async (req, res) => {
+        const auth = new google.auth.GoogleAuth({
+            keyFile: "credentials.json",
+            scopes: "https://www.googleapis.com/auth/spreadsheets",
+        })
+    
+        // Create client instance for auth
+        const client = await auth.getClient();
+    
+        // Instance of Google Sheets API
+        const googleSheets = google.sheets({version: "v4", auth: client});
+    
+        const spreadsheetId = "1oSz2GirT0q9jaIurljRSZQDla6WNESmAIBDbJFC-guc";
+    
+        // Write
+        googleSheets.spreadsheets.values.append({
+            auth,
+            spreadsheetId,
+            range: "Sheet1!A:D",
+            valueInputOption: "RAW",
+            resource: {
+                values: usefulData,
+            },
+        });
+    
+        res.send("Hi!");
     });
- 
-    res.send("Hi!");
+    
+    
+    app.listen(1337, (req, res) => console.log("running on 1337"));
 });
- 
- 
-app.listen(1337, (req, res) => console.log("running on 1337"));
-});
-
-
-
